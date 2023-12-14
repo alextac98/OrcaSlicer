3cfbba47c9c3f3d32a4c4dabf38fd3cf4992fe42
Alex Tacescu <alextac98@gmail.com>
2023-12-13 19:13:20 -0800

    working building with docker

--- BuildLinux_docker.sh
+++ BuildLinux_docker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-set -e # exit on first error
+
+# exit on first error
+set -e
 
 # Build Dockerfile if available
 docker build --tag orca-slicer-builder docker-build/.
+docker run -it --rm -v .:/app orca-slicer-builder /bin/bash -c './BuildLinux.sh -dsir'
